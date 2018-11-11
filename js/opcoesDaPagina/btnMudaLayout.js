7d4aa4cd85032f2cac1085312f5c9660a85fae80
Italo Moraes Santiago <itu.moraes@gmail.com>
2018-11-11 01:37:26 -0200

    Exercise chapter 12

--- js/opcoesDaPagina/btnMudaLayout.js
+++ js/opcoesDaPagina/btnMudaLayout.js
@@ -1,19 +1,21 @@
-const btn = document.querySelector('#btnMudaLayout')
-const mural = document.querySelector('.mural')
-
-btn.classList.remove('no-js')
-
-function mudaTexto() {
-  if (btn.textContent === 'Blocos') {
-    btn.textContent = 'Linhas'
-  } else {
-    btn.textContent = 'Blocos'
+;(function() {
+  const btn = document.querySelector('#btnMudaLayout')
+  const mural = document.querySelector('.mural')
+  
+  btn.classList.remove('no-js')
+  
+  function mudaTexto() {
+    if (btn.textContent === 'Blocos') {
+      btn.textContent = 'Linhas'
+    } else {
+      btn.textContent = 'Blocos'
+    }
   }
-}
-
-function mudaLayout() {
-  mural.classList.toggle('mural--linha')
-}
-
-btn.addEventListener('click', mudaTexto)
-btn.addEventListener('click', mudaLayout)
+  
+  function mudaLayout() {
+    mural.classList.toggle('mural--linha')
+  }
+  
+  btn.addEventListener('click', mudaTexto)
+  btn.addEventListener('click', mudaLayout)
+})()
