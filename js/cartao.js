579d82727c4a1e923bab9c20458182b01e4719dc
Italo Moraes Santiago <itu.moraes@gmail.com>
2018-11-13 01:21:39 -0200

    Exercise chapter 15

--- js/cartao.js
+++ js/cartao.js
@@ -11,5 +11,14 @@
     cartao.addEventListener('focusout', function() {
       cartao.classList.remove('cartao--focado')
     })
+
+    cartao.addEventListener('change', function(event) {
+      const elementoSelecionado = event.target
+      const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
+
+      if (isRadioTipo) {
+        cartao.style.backgroundColor = elementoSelecionado.value
+      }
+    })
   }
 })()
