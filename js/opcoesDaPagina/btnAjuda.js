d30b3b3c270683eb2eb4212146cf85b712bbb7ac
Italo Moraes Santiago <itu.moraes@gmail.com>
2018-11-18 23:44:00 -0200

    Exercise chapter 21

---
+++ js/opcoesDaPagina/btnAjuda.js
@@ -0,0 +1,22 @@
+;(function() {
+  const btnAjuda = document.querySelector('#btnAjuda')
+
+  btnAjuda.addEventListener('click', function() {
+    const ajudas = [
+      {
+        conteudo: 'Bem vindo ao Ceep',
+        cor: '#F05450'
+      },
+      {
+        conteudo: 'Clique no btn Linhas para mudar o layout',
+        cor: '#92C4EC'
+      }
+    ]
+
+    ajudas.forEach(function(ajuda) {
+      alert(ajuda)
+    })
+  })
+
+  btnAjuda.classList.remove('no-js')
+})()
