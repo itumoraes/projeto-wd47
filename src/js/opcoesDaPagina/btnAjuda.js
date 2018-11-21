a2380dedeba01e5cd34370c0a121ce063b976863
Italo Moraes Santiago <itu.moraes@gmail.com>
2018-11-21 00:38:40 -0200

    Exercise chapter 26 with axios

--- src/js/opcoesDaPagina/btnAjuda.js
+++ src/js/opcoesDaPagina/btnAjuda.js
@@ -1,17 +1,24 @@
 ;(function(mural) {
   const btnAjuda = document.querySelector('#btnAjuda')
+  const api = 'https://ceep.herokuapp.com/cartoes/instrucoes'
 
   btnAjuda.addEventListener('click', function() {
-    const xhr = new XMLHttpRequest()
+    // const xhr = new XMLHttpRequest()
 
-    xhr.open('GET', 'https://ceep.herokuapp.com/cartoes/instrucoes')
-    xhr.responseType = 'json'
-    xhr.send()
-    xhr.addEventListener('load', () => {
-      const obj = xhr.response
-      const ajudas = obj.instrucoes
+    // xhr.open('GET', 'https://ceep.herokuapp.com/cartoes/instrucoes')
+    // xhr.responseType = 'json'
+    // xhr.send()
+    // xhr.addEventListener('load', () => {
+    //   const obj = xhr.response
+    //   const ajudas = obj.instrucoes
 
-      ajudas.forEach(function(ajuda) {
+    //   ajudas.forEach(function(ajuda) {
+    //     mural.adicionaCartao(ajuda)
+    //   })
+    // })
+
+    axios.get(api).then(function(response) {
+      response.data.instrucoes.forEach(function(ajuda) {
         mural.adicionaCartao(ajuda)
       })
     })
